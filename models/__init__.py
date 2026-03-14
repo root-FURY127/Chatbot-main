ee6224ad64645c8a21cf535b02a6b7206f7ffdaa
root-FURY127 <113788611+root-FURY127@users.noreply.github.com>
2026-03-15 02:39:26 +0530

    Delete models directory

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user_model import UserResponse, UserUpdate
-from .consent_model import ConsentResponse, ConsentWithdraw
-from .data_location_model import DataLocation, LocationEntry
-from .logs_model import ComplianceLogResponse
